8944dac36f3e06ba518d4e11d8c7205749e73a99
Kim <TzolkinB@users.noreply.github.com>
2016-03-09 20:27:59 -0500

    React Exercise HW

---
+++ index.jsx
@@ -0,0 +1,61 @@
+import style from './styles/';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+console.log('now running!')
+
+class MessageInput extends React.Component {
+    constructor() {
+        super();
+        this.msgArray = [
+            {text: "hello world", time: "7pm", user: "Bobby"},
+            {text: "hello world", time: "4pm", user: "Edwin"},
+            {text: "hello world", time: "4am", user: "Assaf"}
+        ];
+    }
+
+    clickHandler(event) {
+    	console.log(this.refs.message.value);
+    	this.setState = 
+    		message: '';
+    	};
+    }
+
+    render() {
+        
+        var messageComponents = this.msgArray.map(function (item) {
+            return <li>{item.user + " " + item.time + " " + item.text}</li>
+        })
+        return (
+        	<div>
+	            <ul>{messageComponents}</ul>
+	            <input type='text' ref='message' defaultValue='Hi' />
+	            <button onClick={this.clickHandler.bind(this)}>Click Me</button>
+	        </div>
+            //  <h1>{this.msgArray[0].user}</h1>
+            //  <input className="myClass"></input>
+            //  <button></button>
+            // </div>
+         // var currentUser = this.msgArray[0].user
+         //   if(currentUser)
+        )
+    }
+}
+ 
+
+
+// class MessageInput extends React.Component {
+// 	render() {
+// 		return ( <div>
+// 			<h3> Enter Something Below</h3>
+// 			<input type="text"></input>
+// 			<button> Send </button>
+// 			{list}
+// 			</div>
+			
+// 		)
+// 	}
+// };
+
+var mountPoint = document.querySelector("#app");
+ReactDOM.render(<MessageInput/>, mountPoint);
