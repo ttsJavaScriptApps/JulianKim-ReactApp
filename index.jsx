995e6a0c1b9670cfded76afc22bbd1ecdbb2d83b
Kim <TzolkinB@users.noreply.github.com>
2016-03-09 22:12:34 -0500

    Cannot read property 'value' of null
    Error line 41...

--- index.jsx
+++ index.jsx
@@ -8,39 +8,65 @@ class MessageInput extends React.Component {
     constructor() {
         super();
         this.msgArray = [
-            {text: "hello world", time: "7pm", user: "Bobby"},
-            {text: "hello world", time: "4pm", user: "Edwin"},
-            {text: "hello world", time: "4am", user: "Assaf"}
+            {text: "Hello world", time: "Noon", user: "User1"},
+            {text: "HELLO world", time: "8pm", user: "User2"},
+            {text: "hello world", time: "11am", user: "User3"}
         ];
     }
 
     clickHandler(event) {
     	console.log(this.refs.message.value);
-    	this.setState = 
-    		message: '';
-    	};
-    }
+    	this.setState ({
+          message: event.target.value
+        });
+        console.log(this.state.message);
+    	}
+    
+    handleButton(event) {
+        var newMessages = this.state.messageList.slice();
+        newMessages.push(this.state.message)
+        texthis.setState({
+            message: newMessages
+    })
+    console.log(this.state.message);
+  }
 
-    render() {
-        
-        var messageComponents = this.msgArray.map(function (item) {
+
+  render() {
+     var messageComponents = this.msgArray.map(function (item) {
             return <li>{item.user + " " + item.time + " " + item.text}</li>
         })
-        return (
-        	<div>
-	            <ul>{messageComponents}</ul>
-	            <input type='text' ref='message' defaultValue='Hi' />
-	            <button onClick={this.clickHandler.bind(this)}>Click Me</button>
-	        </div>
+    return (
+      <div>
+       // error: Uncaught TypeError: Cannot read property 'value' of null
+        <input type="text" ref="message" value={this.state.value} defaultValue="" />
+        <button onClick={this._clickHandler.bind(this)}>Click Me</button>
+        <ul>{}</ul>
+      </div>
+    );
+  }
+}
+
+    // render() {
+        
+    //     var messageComponents = this.msgArray.map(function (item) {
+    //         return <li>{item.user + " " + item.time + " " + item.text}</li>
+    //     })
+    //     return (
+    //     	<div>
+	   //          <ul>{messageComponents}</ul>
+	   //          <input type='text' ref='message' defaultValue='Hi' />
+	   //          <button onClick={this.clickHandler.bind(this)}>Click Me</button>
+	   //      </div>
             //  <h1>{this.msgArray[0].user}</h1>
             //  <input className="myClass"></input>
             //  <button></button>
             // </div>
          // var currentUser = this.msgArray[0].user
          //   if(currentUser)
-        )
-    }
-}
+//         )
+//     }
+// }
  
 
 
